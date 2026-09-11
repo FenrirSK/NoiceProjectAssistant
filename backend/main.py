6770f4c25d1c27774be17c61dad29c8bc50eef72
FenrirSK <shreerajkarmakar98@gmail.com>
2026-09-11 16:38:12 +0530

    add frontend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.voice import router as voice_router
 
 
@@ -7,6 +8,15 @@ app = FastAPI(
     description="Voice-to-command layer for project management software",
     version="1.0.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(voice_router)
 
 @app.get("/")
@@ -20,3 +30,6 @@ def health_check():
     return {
         "status": "healthy"
     }
+    
+
+    
